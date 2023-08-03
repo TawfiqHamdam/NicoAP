7dd5a4857647ab187eedf5b405aa8a84a581b7a3
Nicholas <clerklypizza315@gmail.com>
2023-08-02 22:18:21 -0400

    hell broke luce

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
         System.out.println("nico is dumb");
         System.out.println("tawfiq loves men");
         System.out.println("nico likes men");
+        String sexSquared="orgasm";
+        if(sexSquared == "orgasm"){
+            System.out.println("1861 navy revolver");
+        } else {
+            System.out.println("ice lemonade my neck was drippin");
+        }
     }
 }
