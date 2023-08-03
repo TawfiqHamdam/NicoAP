630f4a4ae3b9bb8c38c07a3c53183ba38d05554a
Tawfiq <tawfiqhamdam@gmail.com>
2023-08-02 22:12:57 -0400

    Nico is dumb

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("nico is dumb");
         System.out.println("tawfiq loves men");
+        System.out.println("nico likes men");
     }
 }
